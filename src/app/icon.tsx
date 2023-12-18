35dcc3ff029edca577b1b1280c9d6bea4c4f08a7
Rebecca Mead <beckyleemead@gmail.com>
2023-12-18 13:56:54 -0500

    WIP: generate all the icons

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,14 +1,33 @@
 import { ImageResponse } from 'next/og'
  
-export const runtime = 'edge'
- 
-export const size = {
-  width: 32,
-  height: 32,
+export function generateImageMetadata() {
+  return [
+    {
+      contentType: 'image/png',
+      size: { width: 32, height: 32 },
+      id: 'xsmall',
+    },
+    {
+      contentType: 'image/png',
+      size: { width: 48, height: 48 },
+      id: 'small',
+    },
+    {
+      contentType: 'image/png',
+      size: { width: 72, height: 72 },
+      id: 'medium',
+    },
+    {
+      contentType: 'image/png',
+      size: { width: 180, height: 180 },
+      id: 'apple-icon',
+    },
+  ]
 }
-export const contentType = 'image/png'
- 
-export default function Icon() {
+
+export const runtime = 'edge'
+
+export default function Icon( { size }: { size: object }) {
   return new ImageResponse(
     (
       <div
