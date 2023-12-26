59be4815dabb70cbc1c095e627c78f1ab2dc9d31
Rebecca Mead <beckyleemead@gmail.com>
2023-12-26 14:00:09 -0500

    Move app/ directory to top level

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
-import Page from "../src/app/page"
+import Page from "../app/page"
 
 describe("Home page", () => {
   it("renders a heading with the text \"gather here.\"", () => {
