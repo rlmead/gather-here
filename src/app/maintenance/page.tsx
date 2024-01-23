431924e7c397efee60856ea3ec573a98f900ce9f
Domenico Gemoli <domenicogemoli@gmail.com>
2024-01-23 20:09:41 +0100

    Closes #29
    - Add maintenance page
    - Add middleware that redirects all requests to maintenance page when the edge config says so
    - Move "Header and footer" layout to (root)/layout.tsx, and subsequently move all existing pages into that page group (https://nextjs.org/docs/app/building-your-application/routing/route-groups)

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-full flex-col items-center mx-auto text-primary bg-secondary">
+      <div className="container flex flex-col items-center">
+        <h1 className="text-7xl mt-16">Under Maintenance</h1>
+        <p className="text-2xl text-center p-5">
+          The website is currently under maintenance. Please check back later.
+        </p>
+      </div>
+    </main>
+  );
+}
---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { get } from "@vercel/edge-config";
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
+
+export async function middleware(req: NextRequest) {
+  let isInMaintenanceMode = false;
+
+  try {
+    isInMaintenanceMode =
+      // Don't show the maintenance page in development
+      process.env.NODE_ENV !== "development" &&
+      // Check Edge Config to see if the maintenance page should be shown
+      ((await get("isInMaintenanceMode")) ?? false);
+  } catch (e) {
+    // Edge Config not accessible, log the error
+    console.error(e);
+  }
+
+  if (isInMaintenanceMode) {
+    req.nextUrl.pathname = `/maintenance`;
+    return NextResponse.rewrite(req.nextUrl);
+  }
+}
