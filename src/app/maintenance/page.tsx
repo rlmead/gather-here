72c7ec01f9b219ae913f133f1a0edfc692999732
Domenico Gemoli <domenicogemoli@gmail.com>
2024-02-17 16:46:09 +0100

    Update maintenance page with new copy
    As per Richard's email, I've updated the maintenance page
    so that it contains more meaningful copy.
    
    I've also imported the tailwind typography package so we can get nice
    style for free by just using the `prose`class

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,12 +1,71 @@
 export default function Page() {
   return (
-    <main className="mx-auto flex min-h-full flex-col items-center bg-secondary text-primary">
-      <div className="container flex flex-col items-center">
-        <h1 className="mt-16 text-7xl">Under Maintenance</h1>
-        <p className="p-5 text-center text-2xl">
-          The website is currently under maintenance. Please check back later.
-        </p>
-      </div>
-    </main>
+    <div className="prose p-8 md:p-12 lg:p-16">
+      <h1>Welcome to Gather Here</h1>
+      <p>The website is currently under construction.</p>
+      <h2>Our Goal</h2>
+      <p>
+        We aim to create an online platform that will match small groups of
+        people by their location and their interests, and support them in
+        planning Gatherings (In Real Life Meetings) supported and stimulated by
+        material from our Library relevant to their interests.
+      </p>
+      <p>We want to help people find meaning, structure, and community.</p>
+      <h2>Our Dreams</h2>
+      <p className="w-full">
+        Our dreams are primarily, to:
+        <ul>
+          <li>Address the problem of loneliness</li>
+          <li>
+            Empower people to engage with their communities and their world
+          </li>
+        </ul>
+        And secondarily, to:
+        <ul>
+          <li>Foster a culture of evidence-based decision making</li>
+          <li>Enable lifelong learning</li>
+        </ul>
+      </p>
+      <h2>Our Motivations</h2>
+      <p>
+        In our age of global wealth and interdependence, we are deprived of
+        human connection.
+      </p>
+      <p>
+        We describe our modern technology as something that connects us, when in
+        many ways it drives us apart. It overwhelms us with information that has
+        been unbound from context and nuance. It gives us many ways to
+        communicate quickly across great distances, driving us to spend less
+        time together physically. It wastes eons of our collective time by
+        distracting us with advertising, untruth, rage, fear, and bottomless
+        pits of content that makes us feel bad about ourselves.
+      </p>
+      <p>
+        We hope to harness the connecting power of internet platforms to enable
+        real-life, in-person human connection.
+      </p>
+      <p>
+        We are currently developing our platform exclusively with volunteers in
+        a number of countries. If you wish to get in contact please use the
+        email below:
+        <ul>
+          <li>
+            Contact -{" "}
+            <a
+              href="mailto:info@gather-here.net"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              info@gather-here.net
+            </a>
+          </li>
+        </ul>
+      </p>
+      <hr />
+      <small className="pb-4">
+        Gather Here is a not for profit registered in the UK as a Community
+        Interest Company 793191
+      </small>
+    </div>
   );
 }
